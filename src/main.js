72621a0a387d71a5639ea63a1e8309eaf9015fa5
niu <814018399@qq.com>
2018-08-07 10:14:00 +0800

    首页显示

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入bootstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+// 导入index.css
+import './assets/css/index.css'
 
 new Vue({
   el: '#app',
