28a672609d6ffbcf35562377a1c33c27f80f976b
niu <814018399@qq.com>
2018-08-07 12:30:07 +0800

    配置路由规则和导航菜单

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,15 +2,26 @@
 import VueRouter from 'vue-router';
 // 导入Vue
 import Vue from 'vue';
+// 导入组件
+import HeroesList from '../views/heroes/List.vue';
+import EquipsList from '../views/equips/List.vue';
+import WeaponsList  from '../views/weapons/List.vue';
 
 // 注册VueRouter插件
 Vue.use(VueRouter);
 
 // 创建路由对象
 var router = new VueRouter({
+
+  // 配置激活的类样式
+  linkExactActiveClass: 'active',
+
   // 路由规则
   routes: [
-
+    { path: '', redirect: '/heroes' },
+    { name: 'heroes', path: '/heroes', component: HeroesList },
+    { name: 'equips', path: '/equips', component: EquipsList },
+    { name: 'weapons', path: '/weapons', component: WeaponsList }
   ]
 });
 
